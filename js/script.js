93ece95715b28df3d0fa6d3e334e80e76886041e
Daniel David <daniel.david@students.ptechnyc.org>
2018-02-14 23:16:15 +0000

    Appending Stuff to the thing

--- js/script.js
+++ js/script.js
@@ -4,7 +4,15 @@
 
 $(document).ready(function(){
   
-  
+  $.ajax({
+  url: "api.giphy.com/v1/stickers/search?q=puppy&api_key=dc6zaTOxFJmzC",
+  method: "GET",
+  success: function(response) {
+    //Log the whole response to the console
+conosole.log(response);
+$(*".row").append("<img src=" + response + ">");
+
+}); 
   
   
 });
