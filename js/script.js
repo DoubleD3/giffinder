a6c4942183472e6b0b82b968c18361d411d80be5
Daniel David <daniel.david@students.ptechnyc.org>
2018-02-28 23:06:28 +0000

    Added Edwin and have gifs on the document

--- js/script.js
+++ js/script.js
@@ -2,17 +2,37 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
-$(document).ready(function(){
-  
-  $.ajax({
-  url: "api.giphy.com/v1/stickers/search?q=puppy&api_key=dc6zaTOxFJmzC",
-  method: "GET",
-  success: function(response) {
-    //Log the whole response to the console
-conosole.log(response);
-$(*".row").append("<img src=" + response + ">");
+$(document).ready(function() {
+function giphyURLWithSearchTerm(searchTerm) {
 
-}); 
-  
-  
+    var url="https://api.giphy.com/v1/stickers/search?q=" + searchTerm +"&api_key=dc6zaTOxFJmzC";
+return url;
+}
+
+function appendImageToBody(srcURL) {
+    // write a function that will append an <img> to the body with the
+    // URL provided in the parameters
+    $('#image').append('<img src=' + srcURL + '>');
+}
+
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    var url = giphyURLWithSearchTerm(searchTerm);
+    console.log(url);
+    $.ajax({
+      url: url,
+      method: "GET",
+      success: function(response) {
+           //console.log(response)
+           var images_url = response.data[0].images.original.url; 
+    console.log(images_url);
+           appendImageToBody(images_url);
+      },
+    }); 
+}
+
+//Add a click handler beloe to call the function when the button is clicked
+$("#button").click(function(){
+    var searchTerm = $('#input').val();
+    callGiphyAPIWithSearchTerm(searchTerm); 
+});
 });
