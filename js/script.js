7ff03f7d5787d39cbb51cc7350886582e73658c4
Daniel David <daniel.david@students.ptechnyc.org>
2018-03-14 21:04:41 +0000

    Works beautifully

--- js/script.js
+++ js/script.js
@@ -3,36 +3,41 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 $(document).ready(function() {
-function giphyURLWithSearchTerm(searchTerm) {
+    function giphyURLWithSearchTerm(searchTerm) {
 
-    var url="https://api.giphy.com/v1/stickers/search?q=" + searchTerm +"&api_key=dc6zaTOxFJmzC";
-return url;
-}
+        var url = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+        return url;
+    }
 
-function appendImageToBody(srcURL) {
-    // write a function that will append an <img> to the body with the
-    // URL provided in the parameters
-    $('#image').append('<img src=' + srcURL + '>');
-}
+    function appendImageToBody(srcURL) {
+        // write a function that will append an <img> to the body with the
+        // URL provided in the parameters
+        $('#image').append('<img src=' + srcURL + '>');
+    }
 
-function callGiphyAPIWithSearchTerm(searchTerm) {
-    var url = giphyURLWithSearchTerm(searchTerm);
-    console.log(url);
-    $.ajax({
-      url: url,
-      method: "GET",
-      success: function(response) {
-           //console.log(response)
-           var images_url = response.data[0].images.original.url; 
-    console.log(images_url);
-           appendImageToBody(images_url);
-      },
-    }); 
-}
+    function callGiphyAPIWithSearchTerm(searchTerm) {
+        var url = giphyURLWithSearchTerm(searchTerm);
+        console.log(url);
+        $.ajax({
+            url: url,
+            method: "GET",
+            success: function(response) {
+                //console.log(response)
+                var images_url = response.data[0].images.original.url;
+                console.log(images_url);
+                appendImageToBody(images_url);
+            },
+        });
+    }
 
-//Add a click handler beloe to call the function when the button is clicked
-$("#button").click(function(){
-    var searchTerm = $('#input').val();
-    callGiphyAPIWithSearchTerm(searchTerm); 
+    //Add a click handler beloe to call the function when the button is clicked
+    $("#button").click(function() {
+        var searchTerm = $('#input').val();
+        clearList();
+        callGiphyAPIWithSearchTerm(searchTerm);
+    });
 });
-});
+
+function clearList(){
+   $('#image').empty();
+}
